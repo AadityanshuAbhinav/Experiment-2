91a11306d82b438d336cb5cf5a467b33747524ec
Aadityanshu Abhinav <124226696+AadityanshuAbhinav@users.noreply.github.com>
2024-02-28 23:01:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def hello_world():
             return render_template("wrong_inputs.html")
             
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False,host='0.0.0.0')
 	
 
 	
